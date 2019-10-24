cc4bb93c5aa5f7e82304cc6ad6e149b067e63962
Raphael Berendes <rbe.extern@starfinanz.de>
2019-10-24 08:51:57 +0200

    Initial Commit

---
+++ SwiftUIAndCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAndCoreData
+//
+//  Created by Raphael Berendes (extern) on 24.10.19.
+//  Copyright © 2019 Raphael Berendes (extern). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
